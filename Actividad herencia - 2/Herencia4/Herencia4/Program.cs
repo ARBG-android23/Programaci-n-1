e83faa01e708024ed5f00edf354ffb948d05fe37
ARBG-android23 <arbg763@gmail.com>
2025-03-05 16:07:37 -0400

    Add files via upload

---
+++ Actividad herencia - 2/Herencia4/Herencia4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Figura
+{
+    public virtual void Dibujar()
+    {
+        Console.WriteLine("Dibujando una figura genérica");
+    }
+}
+
+class Circulo : Figura
+{
+    public override void Dibujar()
+    {
+        Console.WriteLine("Dibujando un círculo");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Figura miFigura = new Circulo();
+        miFigura.Dibujar(); // Salida: Dibujando un círculo
+    }
+}
---
+++ Actividad herencia - 2/Herencia5/Herencia5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+abstract class Animal
+{
+    public abstract void Sonido();
+}
+
+class Gato : Animal
+{
+    public override void Sonido()
+    {
+        Console.WriteLine("El gato maúlla");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Gato miGato = new Gato();
+        miGato.Sonido(); // Salida: El gato maúlla
+    }
+}
